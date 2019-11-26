b80807941a751b2fd22fd6ead5ed4014f6aa5994
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2019-11-26 17:35:31 +0000

    Controllers are filled

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -1,13 +1,28 @@
 class QuestionAnswersController < ApplicationController
+  before_action :set_question_answer, only: [:show, :create]
 	def new
+    @question_answer = QuestionAnswer.new
 	end
 
 	def create
+    @question_answer.save
+    # Need some redirecting
 	end
 
 	def index
+    @question_answers = QuestionAnswer.all
 	end
 
 	def show
 	end
+
+  private
+  def set_question_answer
+    @question_answer = QuestionAnswer.find(params[:id])
+    # authorize @question_answer
+  end
+
+  def question_answer_params
+    params.require(:question_answer).permit(:survey_question_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_110840) do
+ActiveRecord::Schema.define(version: 2019_11_26_114336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "participants", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "survey_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["survey_id"], name: "index_participants_on_survey_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -24,6 +32,32 @@ ActiveRecord::Schema.define(version: 2019_11_26_110840) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "question_answers", force: :cascade do |t|
+    t.bigint "survey_question_id"
+    t.bigint "participant_id"
+    t.integer "response_int"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["participant_id"], name: "index_question_answers_on_participant_id"
+    t.index ["survey_question_id"], name: "index_question_answers_on_survey_question_id"
+  end
+
+  create_table "survey_questions", force: :cascade do |t|
+    t.text "question"
+    t.bigint "survey_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["survey_id"], name: "index_survey_questions_on_survey_id"
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["project_id"], name: "index_surveys_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,6 +72,10 @@ ActiveRecord::Schema.define(version: 2019_11_26_110840) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "participants", "surveys"
   add_foreign_key "projects", "users"
+  add_foreign_key "question_answers", "participants"
+  add_foreign_key "question_answers", "survey_questions"
+  add_foreign_key "survey_questions", "surveys"
+  add_foreign_key "surveys", "projects"
 end
-
