ee367fe4e52e647ebcaeba0d2a35739c59dd54ba
Susanna Jacob <susanna@mgenlearn.com>
2019-11-27 15:58:43 +0000

    Refactoring and Cleaning up of Code

--- app/controllers/survey_questions_controller.rb
+++ app/controllers/survey_questions_controller.rb
@@ -1,95 +1,62 @@
 class SurveyQuestionsController < ApplicationController
   before_action :set_survey_question, only: [:show, :create]
+  before_action :set_token, only: [:destroy, :create, :update]
+
+  def index
+    @survey_questions = SurveyQuestion.all
+  end
+
+  def show
+  end
+
   def new
     @survey_question = SurveyQuestion.new
   end
 
   def create
-      @survey = Survey.find(params[:survey_id])
-      @survey_question = SurveyQuestion.create(survey_question_params)
-      @survey_question.survey = @survey
-      @survey_question.save
+    @survey = Survey.find(params[:survey_id])
+    @survey_question = SurveyQuestion.create(survey_question_params)
+    @survey_question.survey = @survey
+    @survey_question.save
 
-      @token = ENV["TYPEFORM_API_TOKEN"]
+    append_new_question
 
-      @form = RetrieveFormRequest.new(Form.new(id: @survey.typeform_id), token: @token).form
+    @survey_question.typeform_id = @form.blocks.last.id
+    @survey_question.save
 
-      @form.blocks << OpinionScaleBlock.new(title: @survey_question.question)
-
-      @form = UpdateFormRequest.new(@form, token: @token).form
-
-      @survey_question.typeform_id = @form.blocks.last.id
-      @survey_question.save
-
-      redirect_to survey_path(@survey)
+    redirect_to survey_path(@survey)
   end
 
   def edit
-
     @survey = Survey.find(params[:id])
-
     @survey_question = SurveyQuestion.find(params[:format])
 
     render 'surveys/show'
   end
 
   def update
-
-    @token = ENV["TYPEFORM_API_TOKEN"]
-
     @survey_question = SurveyQuestion.find(params[:id])
 
     if @survey_question.update(survey_question_params)
-
-      @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
-
-      @question_index = @form.blocks.index do |thing|
-        thing.id == @survey_question.typeform_id
-      end
-
-      @form.blocks[@question_index].title = @survey_question.question
-
-      @form = UpdateFormRequest.execute(@form, token: @token).form
+      update_form_question
 
       redirect_to survey_path(@survey_question.survey)
-
     else
       render(:edit)
     end
   end
 
   def destroy
-
-    @token = ENV["TYPEFORM_API_TOKEN"]
-
     @survey_question = SurveyQuestion.find(params[:format])
+    delete_form_question
 
-      @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
+    if @survey_question.delete
 
-      @question_index = @form.blocks.index do |thing|
-        thing.id == @survey_question.typeform_id
-      end
-
-      @form.blocks.delete_at(@question_index)
-
-      @form = UpdateFormRequest.new(@form, token: @token).form
-
-      if @survey_question.delete
-
-        redirect_to survey_path(@survey_question.survey)
-
-      else
-        render(:edit)
-      end
+      redirect_to survey_path(@survey_question.survey)
+    else
+      render(:edit)
+    end
   end
-
-
-	def index
-    @survey_questions = SurveyQuestion.all
-	end
-
-	def show
-	end
 
   private
 
@@ -98,7 +65,31 @@ class SurveyQuestionsController < ApplicationController
     # authorize @survey_question
   end
 
+  def set_token
+    @token = ENV["TYPEFORM_API_TOKEN"]
+  end
+
   def survey_question_params
     params.require(:survey_question).permit(:question)
+  end
+
+  def append_new_question
+    @form = RetrieveFormRequest.new(Form.new(id: @survey.typeform_id), token: @token).form
+    @form.blocks << OpinionScaleBlock.new(title: @survey_question.question)
+    @form = UpdateFormRequest.new(@form, token: @token).form
+  end
+
+  def update_form_question
+    @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
+    @q_index = @form.blocks.index { |t| t.id == @survey_question.typeform_id }
+    @form.blocks[@q_index].title = @survey_question.question
+    @form = UpdateFormRequest.execute(@form, token: @token).form
+  end
+
+  def delete_form_question
+    @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
+    @q_index = @form.blocks.index { |t| t.id == @survey_question.typeform_id }
+    @form.blocks.delete_at(@q_index)
+    @form = UpdateFormRequest.new(@form, token: @token).form
   end
 end
