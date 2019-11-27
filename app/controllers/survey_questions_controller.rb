db00c6cb2631c1adf2e58cc49b7310e2a096a705
Susanna Jacob <susanna@mgenlearn.com>
2019-11-27 14:38:46 +0000

    Update and Delete feature is complete

--- app/controllers/survey_questions_controller.rb
+++ app/controllers/survey_questions_controller.rb
@@ -1,24 +1,28 @@
 class SurveyQuestionsController < ApplicationController
   before_action :set_survey_question, only: [:show, :create]
-	def new
-		@survey_question = SurveyQuestion.new
-	end
+  def new
+    @survey_question = SurveyQuestion.new
+  end
 
-	def create
-    @survey = Survey.find(params[:survey_id])
-    @survey_question = SurveyQuestion.create(survey_question_params)
-    @survey_question.survey = @survey
-    @survey_question.save
+  def create
+      @survey = Survey.find(params[:survey_id])
+      @survey_question = SurveyQuestion.create(survey_question_params)
+      @survey_question.survey = @survey
+      @survey_question.save
 
-    @token = ENV["TYPEFORM_API_TOKEN"]
+      @token = ENV["TYPEFORM_API_TOKEN"]
 
-    @form = RetrieveFormRequest.new(Form.new(id: @survey.typeform_id), token: @token).form
+      @form = RetrieveFormRequest.new(Form.new(id: @survey.typeform_id), token: @token).form
 
-    @form.blocks << OpinionScaleBlock.new(title: @survey_question.question)
-    @form = UpdateFormRequest.new(@form, token: @token).form
+      @form.blocks << OpinionScaleBlock.new(title: @survey_question.question)
 
-    redirect_to survey_path(@survey)
-	end
+      @form = UpdateFormRequest.new(@form, token: @token).form
+
+      @survey_question.typeform_id = @form.blocks.last.id
+      @survey_question.save
+
+      redirect_to survey_path(@survey)
+  end
 
   def edit
 
@@ -30,13 +34,53 @@ class SurveyQuestionsController < ApplicationController
   end
 
   def update
+
+    @token = ENV["TYPEFORM_API_TOKEN"]
+
     @survey_question = SurveyQuestion.find(params[:id])
 
     if @survey_question.update(survey_question_params)
+
+      @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
+
+      @question_index = @form.blocks.index do |thing|
+        thing.id == @survey_question.typeform_id
+      end
+
+      @form.blocks[@question_index].title = @survey_question.question
+
+      @form = UpdateFormRequest.execute(@form, token: @token).form
+
       redirect_to survey_path(@survey_question.survey)
+
     else
       render(:edit)
     end
+  end
+
+  def destroy
+
+    @token = ENV["TYPEFORM_API_TOKEN"]
+
+    @survey_question = SurveyQuestion.find(params[:format])
+
+      @form = RetrieveFormRequest.new(Form.new(id: @survey_question.survey.typeform_id), token: @token).form
+
+      @question_index = @form.blocks.index do |thing|
+        thing.id == @survey_question.typeform_id
+      end
+
+      @form.blocks.delete_at(@question_index)
+
+      @form = UpdateFormRequest.new(@form, token: @token).form
+
+      if @survey_question.delete
+
+        redirect_to survey_path(@survey_question.survey)
+
+      else
+        render(:edit)
+      end
   end
 
 
@@ -49,13 +93,10 @@ class SurveyQuestionsController < ApplicationController
 
   private
 
-<<<<<<< HEAD
-=======
   def set_survey_question
-    @survey_question = SurveyQuestion.find(params[:id])
+    # @survey_question = SurveyQuestion.find(params[:id])
     # authorize @survey_question
   end
->>>>>>> master
 
   def survey_question_params
     params.require(:survey_question).permit(:question)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_180459) do
+ActiveRecord::Schema.define(version: 2019_11_27_123044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_11_26_180459) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
+    t.date "start_date"
+    t.date "end_date"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_180459) do
     t.bigint "survey_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "typeform_id"
     t.index ["survey_id"], name: "index_survey_questions_on_survey_id"
   end
 
