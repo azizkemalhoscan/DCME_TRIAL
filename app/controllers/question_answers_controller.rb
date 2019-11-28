2b4a33ef5cba7896960a0257bd5bcf89868d16ad
Nicolas De Mil <nicodemil@hotmail.com>
2019-11-28 13:52:41 +0000

    iterations fixed

--- app/controllers/question_answers_controller.rb
+++ app/controllers/question_answers_controller.rb
@@ -38,12 +38,12 @@ class QuestionAnswersController < ApplicationController
     @url += "forms/#{@survey.typeform_id}/responses"
     response = RestClient.get(@url, Authorization: "bearer #{@token}")
     @response = JSON.parse(response)
-    if @response["items"].any?
-      @response["items"].each do |item|
-        unless Participant.where(email: item["answers"][0]["text"]).length > 0
-          Participant.create!(email: item["answers"][0]["text"], survey_id: @survey.id)
-        else
-          puts "DIDNT CREATE"
+    @response["items"].each do |item|
+      item["answers"].each do |answer|
+        if answer["type"] == "text"
+          unless Participant.where(email: answer["text"]).length > 0
+            Participant.create!(email: answer["text"], survey_id: @survey.id)
+          end
         end
       end
     end
