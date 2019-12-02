66b09a4cfc2dcb0ab5cbc177140afad7136a45af
Susanna Jacob <susanna@mgenlearn.com>
2019-12-02 15:45:27 +0000

    user show template complete

--- db/migrate/20191129111918_add_firstand_last_name_to_participant_and_user.rb
+++ db/migrate/20191129111918_add_firstand_last_name_to_participant_and_user.rb
@@ -1,7 +1,7 @@
 class AddFirstandLastNameToParticipantAndUser < ActiveRecord::Migration[5.2]
   def change
-  	# add_column :users, :first_name, :string
-  	# add_column :users, :last_name, :string
+  	add_column :users, :first_name, :string
+  	add_column :users, :last_name, :string
   	# add_column :participants, :first_name, :string
   	# add_column :participants, :last_name, :string
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,15 @@ Survey.destroy_all
 
 puts "Creating Users.."
 
-User.create(email: "nick@dcme.today", password: "123456")
-User.create(email: "aziz@dcme.today", password: "123456")
-User.create(email: "susanna@dcme.today", password: "123456")
+User.create(email: "nick@dcme.today", password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+User.create(email: "aziz@dcme.today", password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+User.create(email: "susanna@dcme.today", password: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 
 
 puts "Creating Projects"
 
 User.all.each do |user|
-	2.times do 
+	2.times do
 		Project.create! ({
 			name: Faker::Job.field,
 			user: user
@@ -36,7 +36,7 @@ end
 puts "Creating Surveys"
 
 Project.all.each do |project|
-	1.times do 
+	1.times do
 		Survey.create! ({
 			name: Faker::Job.key_skill,
 			project: project
