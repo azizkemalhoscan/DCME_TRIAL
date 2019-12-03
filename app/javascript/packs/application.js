320c82d989eed3bc72d81384c2f6a92e94957d2f
Nicolas De Mil <nicodemil@hotmail.com>
2019-12-03 17:57:40 +0000

    application JS packs updates and Navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,20 +4,20 @@ import "chart.js/dist/Chart.min.css";
 
 // import "chartkick/dist/chartkick.min.js";
 // import { loadDynamicBannerText } from '../components/banner';
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  console.log('test');
-  if (document.querySelector("#banner-typed-text")) {
-    console.log('test2');
-    new Typed('#banner-typed-text', {
-      strings: ["today!"],
-      typeSpeed: 300,
-      loop: false,
-      showCursor: false
-    });
-  }
-}
+// const loadDynamicBannerText = () => {
+//   console.log('test');
+//   if (document.querySelector("#banner-typed-text")) {
+//     console.log('test2');
+//     new Typed('#banner-typed-text', {
+//       strings: ["today!"],
+//       typeSpeed: 300,
+//       loop: false,
+//       showCursor: false
+//     });
+//   }
+// }
 
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
